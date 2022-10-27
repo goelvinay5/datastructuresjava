33032d3ca442937736ddfc9adc22aa2acab7c109
Vinay Goel <54267249+goelvinay5@users.noreply.github.com>
2022-10-28 01:10:18 +0530

    added crossorigin

--- src/main/java/com/example/datastructures/datastructuresjava/Services/ArrayRotation.java
+++ src/main/java/com/example/datastructures/datastructuresjava/Services/ArrayRotation.java
@@ -1,14 +1,28 @@
 package com.example.datastructures.datastructuresjava.Services;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.datastructures.models.ArrayDS;
- 
+
+@CrossOrigin
 @RestController
+@RequestMapping()
 public class ArrayRotation {
     @GetMapping("/arrayrotation")
     public ArrayDS solve() {
         ArrayDS obj = new ArrayDS();
+        try {
+            obj.problem = new String(Files.readAllBytes(Paths.get("src/main/resources/problems/arrayrotation.txt")));
+            obj.solution = new String(Files.readAllBytes(Paths.get("src/main/resources/solutions/arrayrotation.txt")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        };
         int n =5;
         int[] A = {1,2,3,4,5};
         obj.input = A.clone();
@@ -19,6 +33,7 @@ public class ArrayRotation {
         ar.reverse(A,0,B-1);
         ar.reverse(A,B,n-1);
         obj.Title = "Array Rotation";
+        
         obj.output = A;
         return obj;
     }
